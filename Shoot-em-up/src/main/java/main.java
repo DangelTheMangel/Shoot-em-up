e32ed6a7e8fa170c140ab10fbbb46b73d9778112
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2021-03-15 13:29:36 +0100

    Player can move wugiwugiwugi

--- Shoot-em-up/src/main/java/main.java
+++ Shoot-em-up/src/main/java/main.java
@@ -5,4 +5,32 @@ public class main extends PApplet {
     public static void main(String[] args) {
         PApplet.main("main");
     }
+
+    PlayerShip player = new PlayerShip(this);
+    @Override
+    public void settings() {
+        super.settings();
+        size(1000,1000);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        clear();
+        player.draw();
+    }
+
+    @Override
+    public void keyPressed() {
+        player.controls(key,keyCode,true);
+    }
+
+    @Override
+    public void keyReleased() {
+        player.controls(key,keyCode,false);
+    }
 }
