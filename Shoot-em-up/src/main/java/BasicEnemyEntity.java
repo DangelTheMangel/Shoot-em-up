f8f1c0d4d190a24a42093d6f8d0a1105e67a5569
Christian <christian@randleff.dk>
2021-03-16 13:04:46 +0100

    pew pew ikke done

--- Shoot-em-up/src/main/java/BasicEnemyEntity.java
+++ Shoot-em-up/src/main/java/BasicEnemyEntity.java
@@ -1,8 +1,11 @@
 import processing.core.PApplet;
 import processing.core.PVector;
 
+import java.util.Random;
+
 public class BasicEnemyEntity extends Entity {
-    int turn = 0;
+    int turn =(int) p.random(-10,10);
+    float swing = p.random((float) 0.01,(float) 0.09);
 
     BasicEnemyEntity(PApplet p, PVector position, int xSize, int ySize) {
         super(p, position, xSize, ySize);
@@ -16,10 +19,10 @@ public class BasicEnemyEntity extends Entity {
 
     @Override
     void move() {
-        int sin = (int) (10*p.sin((float) (turn* 0.09)));
+        int sin = (int) (10*p.sin((float) (turn * swing)));
         turn ++;
-        position.x += sin;
-        position.y += 1;
+        position.x += sin/2;
+        position.y += 0.5;
         if(p.height < position.y){
             position.x = p.random(p.width/4,p.width-p.width/4);
             position.y = 0;
